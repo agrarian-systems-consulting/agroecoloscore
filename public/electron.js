dfde3002c70e5537b6caf8f48656b05375cf2b78
Hugo Lehoux <45370179+agrarian-systems@users.noreply.github.com>
2020-12-10 00:15:52 +0100

    Ready for v1.0.0

--- public/electron.js
+++ public/electron.js
@@ -3,8 +3,12 @@ const app = electron.app;
 const BrowserWindow = electron.BrowserWindow;
 const path = require('path');
 const isDev = require('electron-is-dev');
+const updater = require('./updater');
 let mainWindow;
 function createWindow() {
+  // Check for app updates after 3 seconds
+  setTimeout(updater, 3000);
+
   mainWindow = new BrowserWindow({
     width: 1300,
     height: 750,
---
+++ public/updater.js
@@ -0,0 +1,48 @@
+const { dialog } = require('electron');
+const { autoUpdater } = require('electron-updater');
+
+// Configure log debugging
+autoUpdater.logger = require('electron-log');
+autoUpdater.logger.transports.file.level = 'info';
+
+// Disable auto-downloading of updates
+autoUpdater.autoDownload = false;
+// Single export to check for and apply any available updates
+module.exports = () => {
+  // Check for update (GH Releases)
+  autoUpdater.checkForUpdates();
+
+  // Listen for update found
+  autoUpdater.on('update-available', () => {
+    // Prompt user to start download
+    dialog
+      .showMessageBox({
+        type: 'info',
+        title: 'Mise à jour disponible',
+        message:
+          "Une nouvelle version de l'application est disponible. Souhaitez-vous mettre à jour Agroecoloscore ?",
+        buttons: ['Mettre à jour', 'Non'],
+      })
+      .then((result) => {
+        let buttonIndex = result.response;
+        if (buttonIndex === 0) autoUpdater.downloadUpdate();
+      });
+    autoUpdater.downloadUpdate();
+  });
+
+  // Listen for update downloaded
+  autoUpdater.on('update-downloaded', () => {
+    // Prompt the user to install the update
+    dialog
+      .showMessageBox({
+        type: 'info',
+        title: 'Mise à jour est prête à être installée',
+        message: "Souhaitez-vous l'installer et redémarrer Agroecoloscore ?",
+        buttons: ['Oui', 'Plus tard'],
+      })
+      .then((result) => {
+        let buttonIndex = result.response;
+        if (buttonIndex === 0) autoUpdater.quitAndInstall(false, true);
+      });
+  });
+};
